5ef63a347fa72681ba32312099d27580aaca975e
Amaranth01 <vanessa.nectoux@gmail.com>
2022-04-06 17:25:18 +0200

    user manager ok

--- Model/Manager/UserManager.php
+++ Model/Manager/UserManager.php
@@ -19,4 +19,42 @@ class UserManager
             ->setPassword($data['password'])
             ;
     }
+
+    /**
+     * Find a user with their id.
+     * @param int $id
+     * @return User
+     */
+    public static function getUser(int $id): ?User
+    {
+        $result = DB::getPDO()->query("SELECT * FROM user WHERE id = '$id'");
+        return $result ? self::createUser($result->fetch()) : null;
+    }
+
+    public static function mailExist(string $mail): ?array
+    {
+        $stmt = DB::getPDO()->query("SELECT count(*) FROM user WHERE email = '$mail'");
+        return $stmt->fetch();
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public static function addUser(User $user): bool
+    {
+        $stmt = DB::getPDO()->prepare("
+            INSERT INTO user (email, username, password) 
+            VALUES (:email, :username, :password)
+        ");
+
+        $stmt->bindValue('email', $user->getEmail());
+        $stmt->bindValue('username', $user->getUsername());
+        $stmt->bindValue('password', $user->getPassword());
+
+        $result = $stmt->execute();
+        $user->setId(DB::getPDO()->lastInsertId());
+
+        return $result;
+    }
 }
